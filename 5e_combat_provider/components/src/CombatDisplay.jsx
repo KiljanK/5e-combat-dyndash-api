15ee80f9d9a9343d5eab225ad1688ffe8aab841e
Kiljan <50336076+KiljanK@users.noreply.github.com>
2025-08-17 05:26:42 +0200

    Improved Component

--- 5e_combat_provider/components/src/CombatDisplay.jsx
+++ 5e_combat_provider/components/src/CombatDisplay.jsx
@@ -179,6 +179,16 @@ const renderComponent = (uuid, data, slotSettings) => {
 		if (foundEncounter) break;
 	}
 
+	let criticalFailureColor =
+		slotSettings?.colors?.[`general/diceCriticalFailure`] ||
+		"rgba(219, 50, 50, 1)";
+
+	let criticalSuccessColor =
+		slotSettings?.colors?.[`general/diceCriticalSuccess`] ||
+		"rgba(89, 211, 83, 1)";
+
+	let iconColor = "rgba(255, 255, 255)";
+
 	// region rC: Roll Calculation
 
 	// Getting base information from the digitalDice rolls and 5eEncounter information
@@ -200,6 +210,21 @@ const renderComponent = (uuid, data, slotSettings) => {
 	let rollCount = foundDice?.rolls?.length;
 	let previousRoll = foundDice?.rolls[foundDice?.rolls?.length - 2];
 	let currentRoll = foundDice?.rolls[foundDice?.rolls?.length - 1];
+
+	let previousColor = iconColor;
+	let currentColor = iconColor;
+
+	if (previousRoll === 20) {
+		previousColor = criticalSuccessColor;
+	} else if (previousRoll === 1) {
+		previousColor = criticalFailureColor;
+	}
+
+	if (currentRoll === 20) {
+		currentColor = criticalSuccessColor;
+	} else if (currentRoll === 1) {
+		currentColor = criticalFailureColor;
+	}
 
 	// Doing the roll-based calculations and deciding on the shown Elements
 	let choiceFunction = undefined;
@@ -227,6 +252,7 @@ const renderComponent = (uuid, data, slotSettings) => {
 							? "opacity-50 line-through"
 							: ""
 					}`}
+					style={{ color: previousColor }}
 				>{`${previousRoll}`}</code>,
 				<code
 					className={`bg-gray-500 p-1 rounded-md ${
@@ -234,6 +260,7 @@ const renderComponent = (uuid, data, slotSettings) => {
 							? "opacity-50 line-through"
 							: ""
 					}`}
+					style={{ color: currentColor }}
 				>{`${currentRoll}`}</code>
 			);
 		} else {
@@ -241,6 +268,7 @@ const renderComponent = (uuid, data, slotSettings) => {
 			shownRolls.push(
 				<code
 					className={`bg-gray-500 p-1 rounded-md`}
+					style={{ color: currentColor }}
 				>{`${chosenRoll}`}</code>
 			);
 		}
@@ -255,6 +283,48 @@ const renderComponent = (uuid, data, slotSettings) => {
 	}, 0);
 	finalRollResult += statblock_bonus;
 
+	// Creating the rollIcon
+
+	if (chosenRoll === 20) {
+		iconColor = criticalSuccessColor;
+	} else if (chosenRoll === 1) {
+		iconColor = criticalFailureColor;
+	}
+
+	let diceIcon = (
+		<svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+			<path
+				id="d20"
+				fill={iconColor}
+				fillRule="evenodd"
+				stroke="none"
+				d="M 500.534546 936.913269 L 491.717255 936.085815 L 489.588928 934.945679 L 484.116119 932.847656 L 480.163513 930.339294 L 478.439819 929.61615 L 476.007416 928.423828 L 133.868652 731.417725 L 130.569183 728.71814 L 127.982819 727.121948 L 125.725487 725.962891 L 123.516273 722.841553 L 121.319908 719.63269 L 119.033592 716.38147 L 117.893425 712.540161 L 116.981285 710.703491 L 116.786789 289.14447 L 119.962166 283.767761 L 127.362617 274.052673 L 485.028229 66.730042 L 489.892975 63.770874 L 499.622437 63.086731 L 509.351868 63.802002 L 860.828369 266.208618 L 867.669434 269.822083 L 874.724121 275.030273 L 880.881042 283.225647 L 883.17572 290.789978 L 885 500.965973 L 884.087891 707.054932 L 883.17572 709.612244 L 880.881042 716.774414 L 874.683472 724.934692 L 868.996948 729.605103 L 521.855835 929.829834 L 516.953003 932.843262 L 511.480194 934.945679 L 509.351868 936.085815 Z M 479.867065 897.46936 L 482.926849 896.094238 L 485.932434 892.031738 L 486.209534 841.42627 L 483.227722 790.995667 L 477.842804 786.831726 L 228.370361 744.308533 L 225.190247 743.823853 L 223.701218 746.525269 L 226.665665 751.39624 L 476.296753 895.867126 L 478.673035 896.827393 Z M 521.969788 896.300842 L 777.181396 750.485718 L 779.005615 746.306824 L 773.71936 743.703857 L 521.551575 786.770264 L 516.886597 790.121704 L 514.672668 831.666687 L 513.745056 887.442261 L 514.885254 891.741394 L 517.409058 895.626526 Z M 468.638397 756.310303 L 471.490234 754.89563 L 470.198242 751.475159 L 266.478546 399.893066 L 264.747009 396.928589 L 262.986969 399.893066 L 151.499374 697.585632 L 150.878784 701.234192 L 155.757477 703.554504 L 461.312714 754.486023 L 466.329437 755.398132 Z M 529.821106 756.310303 L 531.54718 755.398132 L 842.129578 705.230652 L 843.625305 704.318481 L 846.332703 702.72229 L 847.573425 696.391113 L 846.690308 691.092529 L 737.233948 399.132935 L 736.158936 397.134949 L 734.106628 399.263245 L 532.71875 747.075012 L 529.110779 753.185486 L 528.042358 755.693848 Z M 500.923462 745.614258 L 502.453888 743.081543 L 711.694092 380.510132 L 712.606262 378.685913 L 499.546387 376.861633 L 286.835266 377.545715 L 287.984894 380.054077 L 496.898071 740.734741 L 498.94223 744.702087 Z M 149.058075 619.489868 L 151.648087 617.4375 L 240.119339 379.141968 L 241.031479 374.040955 L 238.006363 366.569702 L 148.726425 313.709961 L 146.218048 312.526123 L 145.865601 618.881775 Z M 851.779541 619.489868 L 853.987366 466.707031 L 852.619141 313.924194 L 851.250977 314.737488 L 764.29718 364.017822 L 761.861633 366.9823 L 760.949463 368.652405 L 760.037354 375.493408 L 760.949463 382.334412 L 848.9729 616.763916 Z M 500.078491 344.024719 C 566.374268 343.426758 698.924194 341.404358 698.924194 341.404358 L 507.998108 105.169861 L 500.840668 101.677795 L 491.413208 106.790344 L 295.322327 344.966675 C 295.322327 344.966675 431.828583 344.64032 500.078491 344.024719 Z M 745.617371 341.207703 L 748.63562 340.37616 L 834.376465 291.120789 L 837.970703 288.775269 L 840.024597 286.94635 L 837.342468 284.829285 L 832.552185 282.05603 L 828.064331 279.221497 L 578.262573 135.202454 L 573.909058 135.704712 L 573.441162 140.215393 L 578.111023 146.098083 L 733.998901 335.356995 L 739.515625 339.155396 Z M 253.469086 339.4104 L 261.584747 338.157104 L 268.612457 332.456238 L 424.727661 142.884766 L 427.107605 138.968506 L 425.804565 135.536438 L 423.26947 134.233398 L 420.722626 136.057678 L 160.961365 287.040161 L 163.697769 290.110596 L 243.919907 336.499573 L 246.048218 338.270264 Z"
+			/>
+		</svg>
+	);
+
+	let displayNumber = finalRollResult;
+	let iconClasses = "";
+
+	if (foundDice?.info?.state === "rolling") {
+		displayNumber = foundDice?.info?.face;
+		iconClasses = "animate-spin";
+	}
+
+	let displayNumberElement = <p className="text-lg">{displayNumber}</p>;
+
+	let rollIcon = (
+		<div className={`w-24 h-24 relative ${iconClasses}`}>
+			{diceIcon}
+			<p
+				className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+				style={{ color: iconColor }}
+			>
+				{displayNumberElement}
+			</p>
+		</div>
+	);
+
 	// region rC: Roll Elements
 
 	// Building the Content for the Roll-Pane
@@ -263,7 +333,7 @@ const renderComponent = (uuid, data, slotSettings) => {
 	// Dice-Based Addends
 	if (foundDice) {
 		resultAddends.push(
-			<div className="text-xs">
+			<div className="flex flex-col justify-end text-xs">
 				<span className="space-x-1">{shownRolls}</span>
 				<p>{foundDice["info"]["name"]}</p>
 			</div>
@@ -285,7 +355,7 @@ const renderComponent = (uuid, data, slotSettings) => {
 			);
 
 			resultAddends.push(
-				<div className="text-xs">
+				<div className="flex flex-col justify-end text-xs">
 					<span className="space-x-1">{e_bonus_element}</span>
 					<p>Encounter</p>
 				</div>
@@ -305,7 +375,7 @@ const renderComponent = (uuid, data, slotSettings) => {
 		);
 
 		resultAddends.push(
-			<div className="text-xs">
+			<div className="flex flex-col justify-end text-xs">
 				<span className="space-x-1">{statblock_bonus_element}</span>
 				<p>{active_statblock}</p>
 			</div>
@@ -315,12 +385,82 @@ const renderComponent = (uuid, data, slotSettings) => {
 	// Getting the actual Result Element
 	let rollPane = (
 		<div className="w-full h-full flex flex-col space-y-2 justify-center items-center">
-			<p className="text-6xl">{finalRollResult}</p>
+			{rollIcon}
 			<span className="flex space-x-2">{resultAddends}</span>
 		</div>
 	);
 
 	// region rC: Encounter Elements
+
+	// Base Information
+	let statblocksURL = e_meta?.toggle;
+	let baseButtonClasses =
+		"px-4 text-white rounded-md transition duration-300 shadow-md hover:shadow-gray-500/50 hover:ring-2 hover:ring-gray-400";
+	let toggleButtonClasses = "w-fit h-fit py-2";
+	let statblockClasses = "w-[90%] h-fit py-3 flex flex-row justify-between";
+
+	// Getter Functions
+	let getClass = (activeBoolean, classes) => {
+		return `${
+			activeBoolean
+				? "bg-blue-600/70 hover:bg-blue-600"
+				: "bg-gray-600/70 hover:bg-gray-600"
+		} ${classes.join(" ")}`;
+	};
+
+	let getToggleClass = (activeBoolean) => {
+		return `${getClass(activeBoolean, [
+			baseButtonClasses,
+			toggleButtonClasses,
+		])}`;
+	};
+
+	let getStatblockClass = (activeBoolean) => {
+		return `${getClass(activeBoolean, [
+			baseButtonClasses,
+			statblockClasses,
+		])}`;
+	};
+
+	let getOnClick = (requestURL, sendObject) => {
+		return async (e) => {
+			e.stopPropagation();
+
+			let requestBody = JSON.stringify(sendObject);
+
+			let requestObject = {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: requestBody,
+			};
+
+			try {
+				const response = await fetch(requestURL, requestObject);
+
+				if (!response.ok) {
+					console.log(
+						`CombatDisplay Component received an error response!`
+					);
+				}
+			} catch {
+				console.log(
+					`CombatDisplay Component failed to get a response!`
+				);
+			}
+		};
+	};
+
+	let getButton = (text, classes, onClick) => {
+		return (
+			<button
+				className={`rounded-md ${classes} z-[2]`}
+				onClick={onClick}
+				key={text}
+			>
+				{text}
+			</button>
+		);
+	};
 
 	let encounterElements = [];
 
@@ -331,87 +471,16 @@ const renderComponent = (uuid, data, slotSettings) => {
 			</div>
 		);
 	} else {
-		// Base Information
-		let requestURL = e_meta?.toggle;
-		let baseButtonClasses =
-			"px-4 text-white rounded-md transition duration-300 shadow-md hover:shadow-gray-500/50 hover:ring-2 hover:ring-gray-400";
-		let toggleButtonClasses = "w-fit h-[60%] py-2";
-		let statblockClasses =
-			"w-[90%] h-fit py-3 flex flex-row justify-between";
-
-		// Getter Functions
-		let getClass = (activeBoolean, classes) => {
-			return `${
-				activeBoolean
-					? "bg-blue-600/70 hover:bg-blue-600"
-					: "bg-gray-600/70 hover:bg-gray-600"
-			} ${classes.join(" ")}`;
-		};
-
-		let getToggleClass = (activeBoolean) => {
-			return `${getClass(activeBoolean, [
-				baseButtonClasses,
-				toggleButtonClasses,
-			])}`;
-		};
-
-		let getStatblockClass = (activeBoolean) => {
-			return `${getClass(activeBoolean, [
-				baseButtonClasses,
-				statblockClasses,
-			])}`;
-		};
-
-		let getOnClick = (sendObject) => {
-			return async (e) => {
-				e.stopPropagation();
-
-				let requestBody = JSON.stringify(sendObject);
-
-				let requestObject = {
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: requestBody,
-				};
-
-				try {
-					const response = await fetch(requestURL, requestObject);
-
-					if (!response.ok) {
-						console.log(
-							`CombatDisplay Component received an error response!`
-						);
-					}
-				} catch {
-					console.log(
-						`CombatDisplay Component failed to get a response!`
-					);
-				}
-			};
-		};
-
-		let getButton = (text, classes, onClick) => {
-			return (
-				<button
-					className={`rounded-md ${classes}`}
-					onClick={onClick}
-					key={text}
-				>
-					{text}
-				</button>
-			);
-		};
-
 		// Actual Buttons, we need to use the e_meta here, since this needs to use the actual values, not the logical values calculated before
 		let advantageClass = getToggleClass(e_meta?.["advantage"]);
-		let advantageClick = getOnClick({
+		let advantageClick = getOnClick(statblocksURL, {
 			encounter: foundEncounterName,
 			advantage: true,
 		});
 		let advantageButton = getButton("A", advantageClass, advantageClick);
 
 		let disadvantageClass = getToggleClass(e_meta?.["disadvantage"]);
-		let disadvantageClick = getOnClick({
+		let disadvantageClick = getOnClick(statblocksURL, {
 			encounter: foundEncounterName,
 			disadvantage: true,
 		});
@@ -430,7 +499,7 @@ const renderComponent = (uuid, data, slotSettings) => {
 				let isActive = active_bonuses?.includes(i);
 
 				let buttonClass = getToggleClass(isActive);
-				let buttonClick = getOnClick({
+				let buttonClick = getOnClick(statblocksURL, {
 					encounter: foundEncounterName,
 					bonus_index: i,
 				});
@@ -468,7 +537,7 @@ const renderComponent = (uuid, data, slotSettings) => {
 			let isActive = statblock === active_statblock;
 
 			let buttonClass = getStatblockClass(isActive);
-			let buttonClick = getOnClick({
+			let buttonClick = getOnClick(statblocksURL, {
 				encounter: foundEncounterName,
 				active_statblock: statblock,
 			});
@@ -498,12 +567,169 @@ const renderComponent = (uuid, data, slotSettings) => {
 
 	// Building the Content for the Encounter-Pane
 	let encounterPane = (
-		<div className="flex flex-col w-full h-full justify-between items-center">
+		<div
+			className="flex flex-col w-full h-full justify-between items-center"
+			onClick={(e) => {
+				e.stopPropagation();
+			}}
+		>
 			{encounterElements}
 		</div>
 	);
 
 	// region rC: Party Elements
+
+	let hitColor =
+		slotSettings?.colors?.["general/hit"] || "rgba(64, 172, 83, 1)";
+
+	let partyElements = [];
+
+	// Cycling through all 5eParty Sources
+	for (let sourceName in data) {
+		if (slotSettings?.exclude?.includes(sourceName)) continue;
+
+		for (let propertyName in data[sourceName]) {
+			if (!propertyName.includes("5eParty")) continue;
+
+			let memberElements = [];
+
+			let party = data[sourceName][propertyName];
+			let party_members = Object.keys(party);
+			let partyURL = party["_meta"]?.toggle;
+
+			// If there are no dice, this source isn't it.
+			if (party_members?.length === 0) break;
+
+			for (let party_member_name of party_members) {
+				let nameIsExcluded = slotSettings?.exclude?.includes(
+					`${sourceName}/${party_member_name}`
+				);
+
+				if (nameIsExcluded || party_member_name === "_meta") {
+					continue;
+				}
+
+				let party_member =
+					data[sourceName][propertyName][party_member_name];
+
+				let pm_bonuses = party_member?.["bonuses"] || [];
+				let pm_active_bonuses = party_member?.["active-bonuses"] || [];
+
+				let pm_active_bonus_values =
+					pm_active_bonuses?.map((index) => pm_bonuses[index]) || [];
+
+				let pm_c_bonuses = party_member?.["custom-bonuses"] || [];
+				let pm_c_active_bonuses =
+					party_member?.["active-custom-bonuses"] || [];
+
+				let pm_c_active_bonus_values =
+					pm_c_active_bonuses?.map((index) => pm_c_bonuses[index]) ||
+					[];
+
+				let finalAC = party_member?.["base-ac"] || 0;
+				finalAC += pm_active_bonus_values.reduce(
+					(accumulator, currentValue) => {
+						return Number(accumulator) + Number(currentValue);
+					},
+					0
+				);
+				finalAC += pm_c_active_bonus_values.reduce(
+					(accumulator, currentValue) => {
+						return Number(accumulator) + Number(currentValue);
+					},
+					0
+				);
+
+				let isHitBy = finalRollResult - finalAC;
+
+				let customHitColor =
+					slotSettings?.colors?.[
+						`${sourceName}/${party_member_name}`
+					] || hitColor;
+
+				let member_color =
+					isHitBy >= 0 ? customHitColor : "rgba(66, 66, 66, 0.33)";
+
+				let member_buttons = [];
+
+				if (pm_bonuses.length > 0) {
+					for (let i = 0; i < pm_bonuses.length; i++) {
+						let bonusValue = pm_bonuses[i];
+						let isActive = pm_active_bonuses?.includes(i);
+
+						let buttonClass = getToggleClass(isActive);
+						let buttonClick = getOnClick(partyURL, {
+							party: sourceName,
+							player: party_member_name,
+							bonus_index: i,
+							bonus_type: "regular",
+						});
+
+						let bonusButton = getButton(
+							`${bonusValue}`,
+							buttonClass,
+							buttonClick
+						);
+
+						member_buttons.push(bonusButton);
+					}
+				}
+
+				let member_element = (
+					<li
+						className="relative w-[90%] h-fit flex flex-row items-center justify-start py-4 rounded-md shadow-lg"
+						style={{ backgroundColor: member_color }}
+					>
+						<span className="flex flex-row w-[30%] flex px-2 justify-between">
+							<p className="">{party_member_name}</p>
+							<code
+								className={`bg-gray-500 p-1 text-xs rounded-md`}
+							>
+								{finalAC}
+							</code>
+						</span>
+						<ul className="w-[70%] flex p-2 justify-end space-x-2">
+							{member_buttons}
+						</ul>
+						{isHitBy > 0 ? (
+							<p
+								className="absolute text-xs px-2 py-1 top-1/2 right-0 translate-x-[70%] -translate-y-1/2 rounded-r-full z-[1]"
+								style={{
+									backgroundColor: member_color,
+								}}
+							>
+								{isHitBy}
+							</p>
+						) : null}
+					</li>
+				);
+
+				memberElements.push(member_element);
+			}
+
+			let partyElement = (
+				<div className="w-full h-fit py-4 bg-gray-700/10 rounded-lg">
+					<p className="py-2 text-xl">{sourceName}</p>
+					<ul className="flex flex-col items-center space-y-2">
+						{memberElements}
+					</ul>
+				</div>
+			);
+
+			partyElements.push(partyElement);
+		}
+	}
+
+	let partyPane = (
+		<div
+			className="flex flex-col w-full h-full overflow-scroll space-y-4 p-2 justify-around items-center"
+			onClick={(e) => {
+				e.stopPropagation();
+			}}
+		>
+			{partyElements}
+		</div>
+	);
 
 	// region rC: General Rendering
 	let backgroundColor =
@@ -525,7 +751,7 @@ const renderComponent = (uuid, data, slotSettings) => {
 				backgroundColor: backgroundColor,
 			}}
 		>
-			<div className="flex flex-col w-1/2 h-full p-4 justify-between">
+			<div className="flex flex-col w-1/3 h-full p-4 justify-between">
 				<div
 					className="w-full h-[30%] rounded-md"
 					style={{
@@ -543,14 +769,14 @@ const renderComponent = (uuid, data, slotSettings) => {
 					{encounterPane}
 				</div>
 			</div>
-			<div className="w-1/2 h-full p-4">
+			<div className="w-2/3 h-full p-4">
 				<div
 					className="w-full h-full rounded-md"
 					style={{
 						backgroundColor: backgroundColorParty,
 					}}
 				>
-					Player Field
+					{partyPane}
 				</div>
 			</div>
 		</div>
@@ -628,7 +854,15 @@ const bundle = {
 	dataTypes: acceptedDataTypes,
 	customSettingsPane: false,
 	settingsMapper: settingsMapper,
-	generalSettings: ["background", "roll", "encounter", "party"],
+	generalSettings: [
+		"background",
+		"roll",
+		"encounter",
+		"party",
+		"hit",
+		"diceCriticalFailure",
+		"diceCriticalSuccess",
+	],
 	dataValidator: dataValidator,
 	renderFunction: renderComponent,
 };
